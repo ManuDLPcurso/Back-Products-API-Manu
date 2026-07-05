6065ebe984f5330abdb4d1838d98c9fc421a8b24
unknown <mdlpa.cursos@gmail.com>
2026-07-05 14:36:54 +0200

    Actualizado ruta absoluta para vercel

--- services/products.service.js
+++ services/products.service.js
@@ -1,5 +1,14 @@
 import fs from "fs/promises";
-const FILE = "./data/products.json";
+import {path} from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILE = path.join(
+  __dirname,
+  "../data/products.json"
+);
+
 export async function getProducts() {
   const data = await fs.readFile(FILE, "utf8");
   return JSON.parse(data);
